f196cd81f7c6cfbb5fc810d5cf8c351914a00254
Cindy Germain <cindymarguerite@gmail.com>
2022-04-17 02:54:15 -0600

    commit tree 02d55e1b18788e4e70a66f0a3b6523507e6af72a

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+      // create an object to keep track of all the numbers we've seen
+      const seenNumbers = {};
+      // iterate over the array of numbers
+      for (const number of array) {
+        // for the current number, identify a complementary number that adds to our target
+        // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+        const complement = target - number;
+        // check if any of the keys in our object is the complement to the current number
+        // if so, return true
+        if (seenNumbers[complement]) return true;
+        // save the current number as the key on our object so we can check it later against other numbers
+        seenNumbers[number] = true;
+      }
+      // if we reach the end of the array, return false
+      return false;
+    }
+
+
 
 /* 
-  Write the Big O time complexity of your function here
+  function hasTargetSum(array, target) {
+  // 1 step
+  const seenNumbers = {};
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    // n steps
+    if (seenNumbers[complement]) return true;
+    // n steps
+    seenNumbers[number] = true;
+  }
+  // 1 step
+  return false;
+}
 */
 
 /* 
-  Add your pseudocode here
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+if we reach the end of the array, return false
 */
 
 /*
